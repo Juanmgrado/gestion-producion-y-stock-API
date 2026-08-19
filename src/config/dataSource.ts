831a191fddedbd3786340fb36af360390ef71f8d
Juanmgrado <Juanmgrado@hotmail.com>
2026-08-19 18:51:33 +0200

    feat: config setted to deploy

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -5,6 +5,8 @@ import { Product } from "../entities/product.entity.js";
 import { StockMovement } from "../entities/stockMovement.entity.js";
 import { StockAdjustment } from "../entities/adjustmentStock.entity.js";
 
+const isProduction = env.NODE_ENV === "production";
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: env.DB_HOST,
@@ -12,10 +14,13 @@ export const AppDataSource = new DataSource({
   username: env.DB_USERNAME,
   password: env.DB_PASSWORD,
   database: env.DB_DATABASE,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
   synchronize: false,
   dropSchema: false,
   logging: false,
   entities: [User, Product, StockMovement, StockAdjustment],
   subscribers: [],
-  migrations: ["src/migrations/**/*.ts"],
+  migrations: [
+    isProduction ? "dist/migrations/**/*.js" : "src/migrations/**/*.ts",
+  ],
 });
