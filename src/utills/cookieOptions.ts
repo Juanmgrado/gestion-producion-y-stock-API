88629f6564ba11184afac876db4e4b07ef344209
Juanmgrado <Juanmgrado@hotmail.com>
2026-08-22 00:25:33 +0200

    update: change cookies oprions

--- src/utills/cookieOptions.ts
+++ src/utills/cookieOptions.ts
@@ -4,13 +4,13 @@ import { ACCESS_TOKEN_COOKIE_MAX_AGE, REFRESH_TOKEN_COOKIE_MAX_AGE } from "./con
 export const ACCESS_TOKEN_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: ACCESS_TOKEN_COOKIE_MAX_AGE,
 };
 
 export const REFRESH_TOKEN_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: REFRESH_TOKEN_COOKIE_MAX_AGE,
 };
