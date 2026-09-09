9e2bfb2bc59c72083626bc2b4f5fd865248b1530
Juanmgrado <Juanmgrado@hotmail.com>
2026-09-09 20:24:06 +0200

    feat: implements auth me

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -8,11 +8,12 @@ import { validateDto } from "../middlewares/validateDto.middleware.js";
 import { LoginUserDto } from "../dto/user/loginUser.dto.js";
 import { verifyToken } from "../middlewares/verifyToken.middleware.js";
 import { ChangePasswordDto } from "../dto/user/changePassword.dto.js";
-import { changeUserPasswordController } from "../controllers/userController.js";
+import { changeUserPasswordController, getCurrentUserController } from "../controllers/userController.js";
 import { loginLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const authRouter = Router();
 
+authRouter.get("/me", verifyToken, getCurrentUserController);
 authRouter.post(
   "/login",
   loginLimiter,
